d04d0f7adc3ba42d63341cdc5ba42493dc499661
Keith Hughitt <keith.hughitt@gmail.com>
2013-10-14 22:03:10 -0400

    Started work on a curses ui

--- lakitu.py
+++ lakitu.py
@@ -13,11 +13,15 @@ import soundcloud
 import pygst
 pygst.require('0.10')
 import gst
-import gobject
+#import gobject
+import curses
 import getpass
+import sys
 import os
 
 password = getpass.getpass()
+
+print("Connecting to SoundCloud...")
 client = soundcloud.Client(client_id='bf7b52c22edb1f76847539356ec563d9',
                            client_secret='b4f0501fd039e73d45883d3f2fbe6ec7',
                            username='user549420240',
@@ -29,9 +33,28 @@ print("Playing " + latest.collection[0]['origin']['title'])
 track = client.get(latest.collection[0]['origin']['uri'])
 stream_url = client.get(track.stream_url, allow_redirects=False)
 
-mainloop = gobject.MainLoop()
-pl = gst.element_factory_make("playbin", "player")
+#mainloop = gobject.MainLoop()
+pl = gst.element_factory_make("playbin2", "player")
 pl.set_property('uri', stream_url.location)
 pl.set_state(gst.STATE_PLAYING)
-mainloop.run()
+
+screen = curses.initscr()
+curses.start_color()
+curses.noecho()
+curses.cbreak()
+screen.keypad(1)
+
+# Main loop
+while 1:
+    c = screen.getch()
+    if c == ord('n'):
+        screen.addstr("Next track", curses.color_pair(2))
+    elif c == ord('q'):
+        curses.nocbreak()
+        screen.keypad(0)
+        curses.echo()
+        curses.endwin()
+        sys.exit()
+
+#mainloop.run()
 #pl.set_state(gst.STATE_PAUSED)
